50e387f827a01b4ae9bbfcd08dbdc72aeb88be1b
Micah Baker <micahb923@gmail.com>
2023-11-01 14:42:32 -0700

    Removed `.only` from unit tests in `testPaGetPayload.js`
    Also added an additional unit test where the ID token submitted is
    unparsable.

--- test/unit/testGoogleHelpers/testPaGetPayload.js
+++ test/unit/testGoogleHelpers/testPaGetPayload.js
@@ -25,9 +25,10 @@ chai.use(chaiAsPromised)
 const sandbox = sinon.createSandbox()
 
 describe('googleHelpers.js unit tests: paGetPayload', () => {
-  /* There are 16 different cases for an ID token (2**4) as there are 4 qualities to check for:
+  /* There are 16 + 1 different cases for an ID token (2**4) as there are 4 qualities to check for:
    * whether the token is expired, whether it originated from PA, whether it is signed by Google, and whether it is for a Brave account.
-   * This four loop generates each test case, and the behaviour expected for each case. */
+   * This four loop generates each test case, and the behaviour expected for each case.
+   * The + 1 test case is where the ID token is empty or absolute gibberish. */
   for (let n = 0; n < 16; n++) {
     const reason = {
       expired: !(Math.floor(n / 8) % 2), // 8 true, 8 false
@@ -44,12 +45,12 @@ describe('googleHelpers.js unit tests: paGetPayload', () => {
 
       // all expired ID tokens should throw an Error
       if (reason.expired) {
-        it.only('should throw an Error for being expired', () => {
+        it('should throw an Error for being expired', () => {
           expect(googleHelpers.paGetPayload(mockIDTokenFactory(reason))).to.be.rejectedWith(Error)
         })
       // if all four qualities of the ID token are met, then paGetPayload should resolve successfully and return a populated payload
       } else if (!reason.expired && !reason.audience && !reason.signature && !reason.profile) {
-        it.only('should resolve successfully and return a populated payload', async () => {
+        it('should resolve successfully and return a populated payload', async () => {
           let error = undefined
           let payload = undefined
 
@@ -64,7 +65,7 @@ describe('googleHelpers.js unit tests: paGetPayload', () => {
         })
       // all non-expired ID tokens should throw a PayloadError with a 'reason' attribute containing why the error was thrown
       } else {
-        it.only(`should throw a PayloadError with reasons ${JSON.stringify(reason)}`, async () => {
+        it(`should throw a PayloadError with reasons ${JSON.stringify(reason)}`, async () => {
           let payloadError = undefined
 
           try {
@@ -80,4 +81,12 @@ describe('googleHelpers.js unit tests: paGetPayload', () => {
       }
     })
   }
+
+  describe('for a completely invalid ID token', () => {
+    it('should throw an Error for being unparsable', () => {
+      expect(googleHelpers.paGetPayload('gibberish')).to.be.rejected
+      expect(googleHelpers.paGetPayload('')).to.be.rejected
+      expect(googleHelpers.paGetPayload(undefined)).to.be.rejected
+    })
+  })
 })
