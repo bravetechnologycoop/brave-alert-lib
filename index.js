51e230c2798bc2df7888e385b054ab4c31830179
Nakul Bansal <nakulbansal727@gmail.com>
2024-12-06 16:41:21 -0800

    Linting

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const { Client, ClientExtension, Session, Device, Gateway, GatewaysVital, ButtonsVital, SensorsVital } = require('./lib/models/index');
-const { ALERT_TYPE, CHATBOT_STATE, DEVICE_TYPE } = require('./lib/enums/index.js');
+const { Client, ClientExtension, Session, Device, Gateway, GatewaysVital, ButtonsVital, SensorsVital } = require('./lib/models/index')
+const { ALERT_TYPE, CHATBOT_STATE, DEVICE_TYPE } = require('./lib/enums/index.js')
 
-const factories = require('./lib/factories');
+const factories = require('./lib/factories')
 
-const helpers = require('./lib/helpers/helpers');
-const twilioHelpers = require('./lib/helpers/twilioHelpers');
-const googleHelpers = require('./lib/helpers/googleHelpers');
+const helpers = require('./lib/helpers/helpers')
+const twilioHelpers = require('./lib/helpers/twilioHelpers')
+const googleHelpers = require('./lib/helpers/googleHelpers')
 
 module.exports = {
   Client,
@@ -15,7 +15,7 @@ module.exports = {
   Gateway,
   GatewaysVital,
   ButtonsVital,
-  SensorsVital,  
+  SensorsVital,
   ALERT_TYPE,
   CHATBOT_STATE,
   DEVICE_TYPE,
@@ -23,4 +23,4 @@ module.exports = {
   helpers,
   twilioHelpers,
   googleHelpers,
-};
+}
