b0374f96ef9b36cdd2bceb61a4da9530aaf560cf
Nakul Bansal <nakulbansal727@gmail.com>
2024-12-06 16:31:49 -0800

    Temporary commit

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-const ALERT_TYPE = require('./lib/alertTypeEnum')
 const AlertSession = require('./lib/alertSession')
 const BraveAlerter = require('./lib/braveAlerter')
-const CHATBOT_STATE = require('./lib/chatbotStateEnum')
+
 const Client = require('./lib/models/Client')
-const DEVICE_TYPE = require('./lib/deviceTypeEnum')
 const Device = require('./lib/models/Device')
-const factories = require('./lib/models/factories')
-const googleHelpers = require('./lib/googleHelpers')
-const helpers = require('./lib/helpers')
 const Session = require('./lib/models/Session')
-const twilioHelpers = require('./lib/twilioHelpers')
+
+const ALERT_TYPE = require('./lib/enums/alertTypeEnum')
+const CHATBOT_STATE = require('./lib/enums/chatbotStateEnum')
+const DEVICE_TYPE = require('./lib/enums/deviceTypeEnum')
+
+const googleHelpers = require('./lib/helpers/googleHelpers')
+const helpers = require('./lib/helpers/helpers')
+const twilioHelpers = require('./lib/helpers/twilioHelpers')
+
+const factories = require('./lib/factories')
 
 module.exports = {
   ALERT_TYPE,
